cbb4c370a2ec09ebe052d314cb9df0a63b5aea0a
luigi94 <luigi.treccozzi@libero.it>
2019-11-13 09:07:36 +0100

    add validation to gui

--- breaker-client/src/main/java/it/unipi/ing/cds/gui/ClientGUI.java
+++ breaker-client/src/main/java/it/unipi/ing/cds/gui/ClientGUI.java
@@ -18,8 +18,6 @@ import java.awt.SystemColor;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.concurrent.Semaphore;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
